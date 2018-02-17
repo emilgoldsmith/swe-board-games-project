17375c831631c76a9836c4257159d4b31c2d9d99
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-02-17 12:11:19 +0400

    Make game compileable and passing initial unit test

--- src/impl/game/ConnectFour.java
+++ src/impl/game/ConnectFour.java
@@ -1,1 +1,55 @@
 package impl.game;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import api.Game;
+import api.Chip;
+
+public class ConnectFour extends Game {
+  // We only store numRows as the global source of truth for the numColumns should
+  // be the length of the board List
+  private int numRows;
+  private List<Chip[]> board;
+  public ConnectFour() {
+    // 6 rows and 7 columns by default, we represent the board as a List of columns
+    this.numRows = 6;
+    int numColumns = 7;
+    board = new ArrayList<Chip[]>();
+    for (int i = 0; i < numColumns; i++) {
+      Chip[] singleColumn = new Chip[numRows];
+      for (int j = 0; j < numRows; j++) {
+        singleColumn[j] = Chip.EMPTY;
+      }
+      board.add(singleColumn);
+    }
+  }
+
+  public int getRows() {
+    return 0;
+  }
+
+  public int getColumns() {
+    return 0;
+  }
+
+  public Chip getChip(int row, int column) {
+    return Chip.EMPTY;
+  }
+
+  public void placeChip(int row, int column) {
+
+  }
+
+  public Chip getWinningPlayer() {
+    return Chip.EMPTY;
+  }
+
+  public Chip getCurrentPlayer() {
+    return Chip.EMPTY;
+  }
+
+  public boolean isGameOver() {
+    return false;
+  }
+}
--- src/impl/view/Console.java
+++ src/impl/view/Console.java
@@ -1,1 +1,20 @@
 package impl.view;
+
+import api.View;
+import api.Game;
+
+import java.util.Observable;
+
+public class Console extends View {
+  public Console(Game game) {
+
+  }
+
+  public void render(Game game) {
+
+  }
+
+  public void update(Observable observable, Object object) {
+
+  }
+}
